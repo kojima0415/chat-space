5c5fb459f987a3e9f3b82460457776824438232f
kojima0415 <kojima0415tsuyoshi@gmail.com>
2019-06-07 13:42:25 +0900

    Update 20190607040315_create_members.rb
    readmeに習って、カラム設定。

--- db/migrate/20190607040315_create_members.rb
+++ db/migrate/20190607040315_create_members.rb
@@ -1,6 +1,8 @@
 class CreateMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :members do |t|
+      t.references :user, foreign_key: true, null: false
+      t.references :group, foreign_key: true, null:false
       t.timestamps
     end
   end
