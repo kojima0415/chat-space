72ea06dc7fa3e550cc5dfeb817dcc1cbbb9bcd72
kojima0415 <kojima0415tsuyoshi@gmail.com>
2019-06-08 13:06:43 +0900

    Update groups_controller.rb
    edit update用に@groupの定義

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+  
   def new
     @group = Group.new
     @group.users << current_user
@@ -22,5 +24,9 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
