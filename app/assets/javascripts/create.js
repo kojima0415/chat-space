cfc755594fb930d6d398fd894496702ad6692a13
kojima0415 <kojima0415tsuyoshi@gmail.com>
2019-06-14 16:28:30 +0900

    Update create.js
    元々のデータから一部の取得したデータに置き換える。
    メッセージの本文と画像は条件によって、記載されるか変化。

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,4 +1,28 @@
 $(document).on('turbolinks:load',function(){
+  function buildHTML(message){
+    var html = `<div class="main-message">
+                  <div class="message-details">
+                    <p class="message-details__user">
+                    ${message.user_name}
+                    </p>
+                    <p class="message-details__informetion">
+                    ${message.created_at}
+                    </p>
+                  </div>
+                  ${message.body == '' ?
+                    ''
+                    :`<p class="main-message__text">
+                        ${message.body}
+                      </p>`
+                  }
+                  ${message.image.url == null ?
+                    ''
+                    :`<img class="lower-message__image" src = ${message.image.url}>`
+                  }
+                  </div>`
+                return html;
+  };
+  
   $(".input-box").on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
