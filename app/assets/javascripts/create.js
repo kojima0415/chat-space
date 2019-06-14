707835112a1c36952cd14e408c1546b09c8ff962
kojima0415 <kojima0415tsuyoshi@gmail.com>
2019-06-14 16:20:14 +0900

    Update create.js
    onイベントをsubmitにて記載。
    
    イベントの着火と既存のform動作を停止。
    formのデータを各種変数化。

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,2 +1,7 @@
 $(document).on('turbolinks:load',function(){
+  $(".input-box").on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+  });
 });
