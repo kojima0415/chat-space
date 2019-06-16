ab25137b8d51c625edeb5d41ba90e3c2917c8463
kojima0415 <kojima0415tsuyoshi@gmail.com>
2019-06-16 16:21:10 +0900

    Update user_search.js
    検索結果のHTMLを挿入する。

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,3 +1,11 @@
+$(document).on('turbolinks:load',function(){
+  function appendhtml_users(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+    $('#user-search-result').append(html);
+  };
   $('#user-search-field').on('keyup', function(){
     var input = $('#user-search-field').val();
     $.ajax({
