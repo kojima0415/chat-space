84d36f2ed92809c6fb153f428a07a8e87db325bd
kojima0415 <kojima0415tsuyoshi@gmail.com>
2019-06-20 00:06:19 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,12 @@ set :repo_url, "git@github.com:kojima0415/chat-space.git"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
 
